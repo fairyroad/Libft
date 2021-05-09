27e7f021e08bee825c7ea47b540d4b67f4fabd75
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-05-09 15:19:57 +0900

    Create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,13 @@
+#include  "libft.h"
+
+void  *ft_memcpy(void *dest,  const void  *src, size_t  num)
+{
+  unsigned  char  *tmp;
+  const unsigned  char  *s;
+  
+  tmp = (unsigned char*)dest;
+  s = (const  unsigned  char*)src;
+  while (num--)
+    *tmp++  = *src++;
+  return  (dest);
+}
