37b3c522c058a9126ee7f8bfd4935eb769fec8f1
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-05-13 21:18:14 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,11 +1,33 @@
 #include  "libft.h"
 
-static  
+static  size_t  ft_strlen(const  char  *s)
+{
+  size_t  num;
+  
+  num = 0;
+  while(s[num])
+  {
+    num++;
+  }
+  return  (num);
+}
+
 char  *ft_strjoin(char  const *s1,  char  const *s2)
 {
   size_t  i;
-  
-  
+  size_t  j;
+  char    *tmp;
+
   i = 0;
-  while (s1[i]
+  j = 0;
+  tmp = (char*)malloc(sizeof(char)  * (ft_strlen(s1)  + ft_strlen(s2) + 1));
+  while (s1[i])
+  {
+    tmp[i]  = s1[i];
+    i++;
+  }
+  while (s2[j])
+    tmp[i++]  = s2[j++];
+  tmp[i]  = '\0';
+  return  (tmp);
 }
