4beb934264da8a5be70118bf728f24d51a518e47
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-05-13 21:20:57 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,7 +20,8 @@ char  *ft_strjoin(char  const *s1,  char  const *s2)
 
   i = 0;
   j = 0;
-  tmp = (char*)malloc(sizeof(char)  * (ft_strlen(s1)  + ft_strlen(s2) + 1));
+  if(!(tmp = (char*)malloc(sizeof(char)  * (ft_strlen(s1)  + ft_strlen(s2) + 1))))
+    return  (NULL);
   while (s1[i])
   {
     tmp[i]  = s1[i];
