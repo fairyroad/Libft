b83442498565f56eb4b292f46d0f1c1c83252fe3
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-06-16 14:56:35 +0900

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,64 +1,61 @@
-#include  "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygil <ygil@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 14:49:53 by ygil              #+#    #+#             */
+/*   Updated: 2021/06/16 14:55:29 by ygil             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static  size_t  trim_len(char  s1, char  const *s2)
+#include "libft.h"
+
+size_t  ft_strlen(const char *s)
 {
-  size_t  i;
-  
-  i = 0;
-  while (s2[i])
-  {
-    if  (s1 ==  s2[i])
-      return  (1);
-    i++;
-  }
-  return (0); 
+    size_t  num;
+
+    num = 0;
+    while (s[num])
+        num++;
+    return (num);
 }
 
-static size_t  mark_firstindex(char const *s1, char const *s2)
+char    *ft_strchr(const char *str, int c)
 {
-  size_t   i;
-  size_t   fpoint;
-  
-  i = 0;
-  fpoint = 0;
-  while (s1[i])
-    if  (trim_len(s1[i++],  s2) ==  1)
-      fpoint++;
-    else
-      return (fpoint);
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == c)
+            return ((char *)str + i);
+        i++;
+    }
+    if (str[i] == c)
+        return ((char *)str + i);
+    return (0);
 }
 
-static size_t mark_lastindex(char const *s1, char const *s2)
+char    *ft_strtrim(char const *s1, char const *set)
 {
-  size_t i;
-  size_t epoint;
-   
-  i = ft_strlen(s1[i]) - 1;
-  epoint = i;
-  while (s1[i])
-    if  (trim_len(s1[i--], s2) == 1)
-      epoint--;
-    else
-      return (epoint);
-}
+    int     start;
+    int     end;
+    int     i;
+    char    *tmp;
 
-char  *ft_strtrim(char  const *s1,  char  const *s2)
-{
-  size_t   i;
-  size_t   fpoint;
-  size_t   epoint;
-  char     *tmp;
- 
-  epoint = mark_lastindex(s1,s2);
-  fpoint  = mark_firstindex(s1,s2);
-  if((fpoint <= epoint) && 
-     !(tmp = (char*)malloc(sizeof(char)  * (epoint  - fpoint  + 1))))
-    return  (NULL);
-  else
-      return ((char*)ft_calloc(sizeof(char), 1);
-  i = 0;
-  while (fpoint <= epoint)
-    tmp[i++]  = s1[fpoint++];
-  tmp[i]  = '\0';
-  return  (tmp);
+    start = 0;
+    end = ft_strlen(s1);
+    i = 0;
+    while (s1[start] && ft_strchr(set, s1[start]))
+        start++;
+    while (s1[end] && ft_strchr(set, s1[end]) && start < end)
+        end--;
+    if (!(tmp = (char*)malloc(sizeof(char*) * (end - start + 1))))
+        return (NULL);
+    while (start <= end)
+        tmp[i] = s1[start];
+    tmp[i] = '\0';
+    return (tmp);
 }
