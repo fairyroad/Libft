c407cfa2fca713aa228b31570f58006c072b587c
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-06-18 16:17:29 +0900

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,12 @@
 /*   By: ygil <ygil@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 16:18:00 by ygil              #+#    #+#             */
-/*   Updated: 2021/06/16 17:02:00 by ygil             ###   ########.fr       */
+/*   Updated: 2021/06/18 15:55:25 by ygil             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static int  number_of_digits(int n)
 {
@@ -25,31 +26,37 @@ static int  number_of_digits(int n)
     return (num);
 }
 
+static int  check_sign(int n)
+{
+    if (n < 0)
+        return (1);
+    return (0);
+}
+
 char        *ft_itoa(int n)
 {
     int     num;
     int     flag;
     char    *tmp;
 
-    flag = 0;
     if (n == 0)
-        return ("0");
+        return ("0\0");
     else if (n == -2147483648)
-        return ("-2147483648");
-    if (n < 0)
-    {
-        flag = 1;
-        n = -n;
-    }
-    num = number_of_digits(-n);
-    if (!(tmp = (char*)malloc(sizeof(char*) * (flag + num) + 1)))
+        return ("-2147483648\0");
+    flag = check_sign(n);
+    num = number_of_digits(n) + flag;
+    if (!(tmp = (char*)malloc(sizeof(char) * num + 1)))
         return (NULL);
     if (flag == 1)
+    {
         tmp[0] = '-';
-    tmp[--num] = '\0';
+        n = -n;
+    }
+    tmp[num--] = '\0';
     while (n)
     {
         tmp[num--] = '0' + n % 10;
         n = n / 10;
     }
+    return (tmp);
 }
