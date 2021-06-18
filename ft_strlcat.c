74866f49888073c1055ef34c9f3613d3fc4b2d90
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-06-18 15:52:27 +0900

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,63 @@
-#include  "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygil <ygil@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 15:55:41 by ygil              #+#    #+#             */
+/*   Updated: 2021/06/18 15:51:57 by ygil             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-size_t  strlcat(char  *dst, const char  *src, size_t  size)
+#include "libft.h"
+
+size_t  ft_strlen(const char *s1)
 {
-  size_t  i;
-  size_t  len_dst;
-  size_t  len_src; 
-  
-  i = 0;
-  while(dst[i])
-    i++;
-  len_dst = i;
-  len_src = 0;
-  if  (size > len_dst + 1)
-  {
-    while((len_src + len_dst + 1 < size) &&  src[i])
-      dst[i++]  = src[len_src++];
-    dst[i]  = '\0';
-    return  (len_src  + len_dst);
-  }
-  else
-    return  (size + len_src);
+    size_t i;
+
+    i = 0;
+    while (s1[i])
+        i++;
+    return (i);
+}
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t i;
+    size_t len_dst;
+    size_t len_src;
+
+    i = 0;
+    len_dst = ft_strlen(dst);
+    len_src = ft_strlen(src);
+    if (size < len_dst + 1)
+        return (size + len_src);
+    if (size > len_dst + 1)
+    {
+        while ((i + 1 + len_dst < size) && src[i] != '\0')
+        {
+            dst[len_dst + i] = src[i];
+            i++;
+        }
+    }
+    dst[len_dst + i] = '\0';
+    return (len_src + len_dst);
+}
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    char *dest = (char *)malloc(sizeof(char) * 15);
+    dest[14] = 'a';
+    memset(dest, 0, 15);
+    memset(dest, 'r', 6);
+
+    char *src = "lorem ipsum dolor sit amet";
+    size_t size = 15;
+    size_t tmp = ft_strlcat(dest, src, size);
+    printf("%zu\n", tmp);
+    printf("%s", dest);
 }
