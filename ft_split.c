9565d8530cba132a6117de6c8fa51d9d0402c52e
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-06-21 23:58:48 +0900

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -96,3 +96,111 @@ int     main(void)
     }
     return (0);
 }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//2021-06-21 
+//#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+static int      cnt_arraysize(char const* s, char c)
+{
+    int     i;
+    int     cnt;
+    int     flag;
+
+    cnt = 0;
+    i = 0;
+    flag = 1;
+    while (s[i])
+    {
+        while (s[i] == c && s[i++])
+            flag = 1;
+        if (flag == 1 && s[i])
+            cnt++;
+        while (s[i] != c && s[i])
+            i++;
+        flag = 0;
+    }
+    return (cnt);
+}
+
+static int      cnt_arrayinsize(char const* s, char c, int i)
+{
+    int     cnt;
+
+    cnt = 0;
+    while (s[i] != c && s[i++])
+        cnt++;
+    return (cnt);
+}
+
+static char** clear_tmp(char** tmp)
+{
+    unsigned int    i;
+
+    i = 0;
+    while (tmp[i])
+        free(tmp[i++]);
+    free(tmp);
+    return (NULL);
+}
+
+static char* put_arrayinsize(char const* s, int start, int end)
+{
+    char* tmp;
+    int   i;
+
+    i = 0;
+    if (!(tmp = (char*)malloc(sizeof(char) * end + 1)))
+        return (NULL);
+    while (end)
+    {
+        tmp[i++] = s[start++];
+        end--;
+    }
+    tmp[i] = '\0';
+    return (tmp);
+}
+
+char** ft_split(char const* s, char c)
+{
+    char** tmp;
+    int     cnt;
+    int     j;
+    int     end;
+    int     start;
+
+    cnt = cnt_arraysize(s, c);
+    if (!(tmp = (char**)malloc(sizeof(char*) * cnt + 1)))
+        return (NULL);
+    j = 0;
+    start = 0;
+    while (j < cnt)
+    {
+        while (s[start] == c && s[start++]);
+        end = cnt_arrayinsize(s, c, start);
+        if (!(tmp[j++] = put_arrayinsize(s, start, end)))
+            return (clear_tmp(tmp));
+        start = start + end;
+    }
+    tmp[j] = NULL;
+    return (tmp);
+}
+
+int     main(void)
+{
+    int i = 0;
+    char** tab;
+
+    //tab = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse", ' ');
+    //tab = ft_split("   lorem   ipsum dolor     sit amet,    ", ' ');
+    tab = ft_split("        ", ' ');
+    while (tab[i] != NULL)
+    {
+        printf("string %d : %s\n", i, tab[i]);
+        i++;
+    }
+    if (tab[i] == NULL)
+        printf("hahaha");
+    return (0);
+}
