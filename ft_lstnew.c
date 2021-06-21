dedadc43d7315c1b25065dc9c1a2e9dfeb9b61d0
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-06-22 00:21:39 +0900

    Create ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,9 @@
+t_list *ft_lstnew(void *content)
+{
+  t_list *tmp;
+  if (!(tmp = (t_list*)malloc(sizeof(t_list))))
+    return (NULL);
+  tmp -> content = content;
+  tmp -> next = NULL;
+  return (tmp);
+}
